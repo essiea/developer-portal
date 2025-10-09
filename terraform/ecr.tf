9c539379e73e19c79544e3f0a9e54bc076c5e815
EC2 Default User <ec2-user@ip-10-0-0-33.ec2.internal>
2025-10-09 21:58:15 +0000

    Image tagging update

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,8 +1,67 @@
 resource "aws_ecr_repository" "frontend" {
   name = "${var.project_name}-frontend"
   force_delete = true
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
+
+resource "aws_ecr_lifecycle_policy" "frontend_policy" {
+  repository = aws_ecr_repository.frontend_repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only last 10 image SHAs + latest",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["sha", "latest"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 10
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_ecr_repository" "backend" {
   name = "${var.project_name}-backend"
   force_delete = true
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
+resource "aws_ecr_lifecycle_policy" "backend_policy" {
+  repository = aws_ecr_repository.backend_repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only last 10 image SHAs + latest",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["sha", "latest"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 10
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
